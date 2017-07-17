c58a6358144619c7f47328936706143d896d407a
Michael Pearce <michael.pearce@peak.bi>
2017-07-17 11:35:06 +0100

    initial commit
    initial commit of bash scripts, unintegrated with awscli at present

---
+++ README.md
@@ -0,0 +1,25 @@
+# aws-encrypt-creds
+
+# about aws-encrypt-creds
+* A experimental way of encrypting awscli credentials while not in use, and decrypt as needed
+* Checks if there is an env var set for a shared credentials file, if not uses default location
+* Checks file type to avoid encrypting/decrypting files unnecessarily
+
+# prerequisites & assumptions
+* You have installed and configured awscli (used for describe-instances api calls etc.)
+* Required ssh keys are held at ~/.ssh/ or set with the env var AWS_SHARED_CREDENTIALS_FILE
+* you have openssl installed
+
+# usage
+* `aws-encrypt-creds encrypt`
+* `aws-encrypt-creds decrypt`
+
+# further usage
+* You may choose to run the script as part of your shell aliases so that you don't have to type the same parameters each time
+
+# disclaimer
+* For personal use and development - may not work for you, but feel free adapt as necessary
+
+# Ideas
+* Integrate with awscli - either using bash alias or editing awscli source
+* Is there anyway to run this headless? Currently requires user intervention for password input
---
+++ aws-encrypt-creds.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function encryptcreds(){
+if [ -z ${AWS_SHARED_CREDENTIALS_FILE} ]; then
+  echo "creds env var not set, using default location"
+  type=$(file ~/.aws/credentials | cut -f2 -d' ')
+  echo "file type is $type"
+  if [ $type = data ]; then
+    echo "Already encrypted, nothing to do"
+  else
+  openssl enc -aes-256-cbc -salt -in ~/.aws/credentials -out ~/.aws/credentials.enc && mv ~/.aws/credentials.enc  ~/.aws/credentials;
+  fi
+else
+  echo ${AWS_SHARED_CREDENTIALS_FILE}
+  type=$(file $AWS_SHARED_CREDENTIALS_FILE | cut -f2 -d' ')
+  echo "file type is $type"
+  if [ $type = data ]; then
+    echo "Already encrypted, nothing to do"
+  else
+  openssl enc -aes-256-cbc -salt -in "$AWS_SHARED_CREDENTIALS_FILE" -out "$AWS_SHARED_CREDENTIALS_FILE".enc && mv "$AWS_SHARED_CREDENTIALS_FILE".enc  "$AWS_SHARED_CREDENTIALS_FILE";
+  fi
+
+fi
+}
+
+decryptcreds() {
+if [ -z ${AWS_SHARED_CREDENTIALS_FILE} ]; then
+  echo "creds env var not set, using default location"
+  type=$(file ~/.aws/credentials | cut -f2 -d' ')
+  echo "file type is $type"
+  if [ $type = ASCII ]; then
+    echo "Already decrypted, nothing to do"
+  else
+  openssl enc -aes-256-cbc -d -in ~/.aws/credentials -out ~/.aws/credentials.unenc && mv ~/.aws/credentials.unenc ~/.aws/credentials;
+  fi
+else
+  echo "$AWS_SHARED_CREDENTIALS_FILE"
+  type=$(file $AWS_SHARED_CREDENTIALS_FILE | cut -f2 -d' ')
+  echo "file type is $type"
+  if [ $type = ASCII ]; then
+    echo "Already decrypted, nothing to do"
+  else
+  openssl enc -aes-256-cbc -d -in $AWS_SHARED_CREDENTIALS_FILE -out $AWS_SHARED_CREDENTIALS_FILE.unenc && mv $AWS_SHARED_CREDENTIALS_FILE.unenc $AWS_SHARED_CREDENTIALS_FILE;
+  fi
+fi
+}
+
+if [ "$1" = "encrypt" ] ; then
+  encryptcreds
+else
+  decryptcreds
+fi
